5daf93c768a998d22e6af72b0033f8dbf265244d
Sephirosu <irfansahman88@gmail.com>
2024-06-14 15:55:07 +0200

    Fixed deployment issue

--- src/app/CartContext.jsx
+++ src/app/CartContext.jsx
@@ -5,17 +5,12 @@ const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState(() => {
-    if (typeof window !== "undefined") {
-      const savedCartItems = localStorage.getItem("cartItems");
-      return savedCartItems ? JSON.parse(savedCartItems) : [];
-    }
-    return [];
+    const savedCartItems = localStorage.getItem("cartItems");
+    return savedCartItems ? JSON.parse(savedCartItems) : [];
   });
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      localStorage.setItem("cartItems", JSON.stringify(cartItems));
-    }
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
   }, [cartItems]);
 
   return (
